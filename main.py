7c7b1578e50bb082015864b2f41a34e3bd76c583
Takkakynttila <42032491+Takkakynttila@users.noreply.github.com>
2021-09-11 18:39:08 +0300

    Capitalization of stock LSE for plot
    -

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    stock_name = input('Give name of stock to plot: ') # for trial purposes need to make adaptive later
+    stock_name = input('Give LSE: ') # for trial purposes need to make adaptive later
     stock_ticker = yf.Ticker(stock_name)
     stock_ticker.info
     history = stock_ticker.history(period = "max")
 
     plt.plot(history['High'])
     plt.plot(history['Low'])
+    plt.title(stock_name.upper())
     plt.show()
 
 if __name__ == "__main__":
     main()
+
