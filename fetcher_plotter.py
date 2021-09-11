9ef3a12edb672fe6db3886f0011830a31721845d
Takkakynttila <42032491+Takkakynttila@users.noreply.github.com>
2021-09-11 21:03:08 +0300

    Added dynamic period changing

--- fetcher_plotter.py
+++ fetcher_plotter.py
@@ -2,14 +2,16 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 class plotter():
-    def __init__(self, stock_lse): # initiate class with parameters and create required ticker(s) for yfinance
+    def __init__(self, stock_lse, period = 'max'): # initiate class with parameters and create required ticker(s) for yfinance
         self.lse = stock_lse
+        self.period = period
         self.ticker = yf.Ticker(self.lse)
         self.ticker.info
-        self.history = self.ticker.history(period = 'max')
+        self.history = self.ticker.history(period = self.period)
 
     def plot(self): #plot with high and low prices
         plt.plot(self.history['High'])
         plt.plot(self.history['Low'])
-        plt.title(self.lse.upper())
+        plt.title(self.lse.upper() + ', ' + self.period)
         plt.show()
+
