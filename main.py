16ff8255858167642dfe57cf5b3cfc6218d99187
Takkakynttila <42032491+Takkakynttila@users.noreply.github.com>
2021-09-11 21:04:01 +0300

    Added dynamic period selection
    This contains the ui related stuff of the dynamic period selection

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import fetcher_plotter as fp
 
 def main():
     stock_name = input('Give LSE: ')
-    stock = fp.plotter(stock_name)
+    stock_period = input('Give optional period (1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max) - max by default: ')
+    if stock_period == None:
+        stock = fp.plotter(stock_name)
+    else:
+        stock = fp.plotter(stock_name, stock_period)
+
     stock.plot()
 
 if __name__ == "__main__":
     main()
+
